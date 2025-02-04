87d429643e84a592e6e7ea9c4a45ac91666b2023
pavithra <pavithra.25cs@licet.ac.in>
2025-02-04 23:17:06 +0530

    First commit

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "local" {}
+
+resource "null_resource" "install_iis" {
+  provisioner "local-exec" {
+    command = "powershell.exe Install-WindowsFeature -Name Web-Server -IncludeManagementTools"
+  }
+}
+
+resource "null_resource" "deploy_website" {
+  depends_on = [null_resource.install_iis]
+  provisioner "local-exec" {
+    command = "xcopy /s /y website C:\\inetpub\\wwwroot\\mywebsite"
+  }
+}
+
+resource "null_resource" "restart_iis" {
+  depends_on = [null_resource.deploy_website]
+  provisioner "local-exec" {
+    command = "iisreset"
+  }
+}
